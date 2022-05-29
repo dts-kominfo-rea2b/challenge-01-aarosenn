91857f97863a03a7cc8ef0f1ddf317364926ffd7
Anggoro Setyawan <anggoros90@gmail.com>
2022-05-29 14:36:32 +0700

    pesan

--- index.js
+++ index.js
@@ -69,7 +69,8 @@ const secondUser = {
 
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
-const users = [firstUser, secondUser];
+const users = [];
+users.push(firstUser, secondUser);
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
